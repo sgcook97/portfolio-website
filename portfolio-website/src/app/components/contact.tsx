8f0c5a9f35a6f86f0c26877f13c6866855604c6b
Sam Cook <opp11@txstate.edu>
2024-05-21 00:24:24 -0500

    added toast and cleared form on successful email

--- portfolio-website/src/app/components/contact.tsx
+++ portfolio-website/src/app/components/contact.tsx
@@ -1,31 +1,40 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { sendEmail } from "@/actions/sendEmail";
-
-import LinkedInLink from "./linkedin-link";
-import GitLink from "./git-link";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact() {
+    const formRef = useRef<HTMLFormElement>(null);
 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (formRef.current) {
+            const formData = new FormData(formRef.current);
+            try {
+                await sendEmail(formData);
+                formRef.current.reset();
+                toast.success("Your message has been sent successfully!", {
+                    position: 'top-center'
+                });
+            } catch (error) {
+                toast.error("There was an error sending your message. Please try again.", {
+                    position: 'top-center'
+                });
+            }
+        }
+    };
     return (
         <div className="flex flex-col justify-center items-center w-[min(100%, 40rem)] mb-[4rem]">
             <h1 className="font-semibold text-[34px]">Contact me!</h1>
             <p className="text-[16px] text-center">
                 Please email me at <a className="underline hover:pointer font-medium hover:text-slate-500" href="mailto:sgcook97@gmail.com">sgcook97@gmail.com</a> or contact me below:
             </p>
-            {/* <div className="flex justify-center items-center">
-                <LinkedInLink />
-                <GitLink gitURL="https://github.com/sgcook97" iconColor="text-slate-900" />
-            </div> */}
-                
-
-            {/* FIX ME */}
             <form className="flex flex-col justify-center items-end w-full mt-8" 
-                action={async (formData) => {
-                    await sendEmail(formData);
-                }}
+                ref={formRef}
+                onSubmit={handleSubmit}
             >
                 <input className="w-full opacity-100 bg-slate-200 h-10 border border-black/10 rounded-lg pl-2 py-1
                     focus:border-slate-700 focus:border-[3px] focus:outline-none"
@@ -43,7 +52,8 @@ export default function Contact() {
                 >
                     Send <FaRegPaperPlane className="transition group-hover:translate-x-[2px] group-hover:-translate-y-[3px] ml-2" />
                 </button>  
-            </form>       
+            </form>  
+            <ToastContainer />     
         </div>
     );
 }
