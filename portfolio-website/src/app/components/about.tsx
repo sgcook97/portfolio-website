ee1a1d6543c3deaf068e6274859383cb83e1ab3f
Sam Cook <opp11@txstate.edu>
2025-02-07 18:34:00 -0600

    updated about info

--- portfolio-website/src/app/components/about.tsx
+++ portfolio-website/src/app/components/about.tsx
@@ -11,10 +11,10 @@ export default function About() {
             <div className="min-w-[300px] flex flex-wrap justify-center w-full text-center items-center max-w-[750px]">
                 <p>Hey there! I'm Sam Cook, a recent Computer 
                     Science graduate from Texas State University, 
-                    currently based in Austin, Texas. I have a 
-                    passion for coding and problem-solving, with 
-                    experience in various software projects during 
-                    my studies. Outside of coding, you can find me 
+                    currently based in Austin, Texas. I'm very passionate about 
+                    coding and have gained hands-on experience 
+                    through numerous academic and personal projects.
+                    Outside of coding, you can find me 
                     enjoying volleyball or diving into the 
                     latest video game releases. Let's connect and 
                     discuss how we can collaborate on exciting 
